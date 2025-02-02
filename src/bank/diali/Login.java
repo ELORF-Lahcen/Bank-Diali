1c91146fa062c33437b2728384888b0c2b23e52d
Lahcen ELORF <lahcenelorf.24@gmail.com>
2025-02-02 19:26:52 +0100

    finish the login part

--- src/bank/diali/Login.java
+++ src/bank/diali/Login.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Login {
+package bank.diali;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Login extends JFrame {
+
+    Login() {
+        super("Bank Diali");
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/bank.png"));
+        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(350, 10, 100, 100);
+        add(image);
+
+        ImageIcon ii1 = new ImageIcon(ClassLoader.getSystemResource("icons/card.png"));
+        Image ii2 = ii1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon ii3 = new ImageIcon(ii2);
+        JLabel iimage = new JLabel(ii3);
+        iimage.setBounds(630, 350, 100, 100);
+        add(iimage);
+
+        setLayout(null);
+        setSize(850, 480);
+        setLocation(280, 160);
+
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
 }
