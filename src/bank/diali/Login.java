b8b1e3cc2f759a36dbc8504d7f3e42b4c97daf72
Lahcen ELORF <lahcenelorf.24@gmail.com>
2025-02-04 19:12:07 +0100

    solve some problems

--- src/bank/diali/Login.java
+++ src/bank/diali/Login.java
@@ -5,6 +5,8 @@ import java.awt.*;
 
 public class Login extends JFrame {
 
+    JLabel label1, label2, label3;
+
     Login() {
         super("Bank Diali");
 
@@ -21,6 +23,29 @@ public class Login extends JFrame {
         JLabel iimage = new JLabel(ii3);
         iimage.setBounds(630, 350, 100, 100);
         add(iimage);
+
+        label1 = new JLabel("WELCOME TO ATM");
+        label1.setForeground(Color.WHITE);
+        label1.setFont(new Font("AvantGarde", Font.BOLD, 38));
+        label1.setBounds(230,125,450,40);
+        add(label1);
+
+        label2 = new JLabel("Card No:");
+        label2.setFont(new Font("Ralway", Font.BOLD, 28));
+        label2.setForeground(Color.WHITE);
+        label2.setBounds(150, 190, 375, 30);
+        add(label2);
+
+
+
+        ImageIcon iii1 = new ImageIcon(ClassLoader.getSystemResource("icons/backbg.png"));
+        Image iii2 = iii1.getImage().getScaledInstance(850, 480, Image.SCALE_DEFAULT);
+        ImageIcon iii3 = new ImageIcon(iii2);
+        JLabel iiimage = new JLabel(iii3);
+        iiimage.setBounds(0, 0, 850, 480);
+        add(iiimage);
+
+
 
         setLayout(null);
         setSize(850, 480);
