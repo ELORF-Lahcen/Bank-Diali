9a2feed7479636d089bc2bd204ceb1aced78445d
Lahcen ELORF <lahcenelorf.24@gmail.com>
2025-02-05 17:44:26 +0100

    Add Events to the Login part

--- src/bank/diali/Login.java
+++ src/bank/diali/Login.java
@@ -2,10 +2,15 @@ package bank.diali;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Login extends JFrame {
+public class Login extends JFrame implements ActionListener {
 
     JLabel label1, label2, label3;
+    JTextField textField2;
+    JPasswordField passwordField3;
+    JButton button1, button2, button3;
 
     Login() {
         super("Bank Diali");
@@ -36,7 +41,45 @@ public class Login extends JFrame {
         label2.setBounds(150, 190, 375, 30);
         add(label2);
 
+        textField2 = new JTextField(15);
+        textField2.setBounds(325, 190, 230, 30);
+        textField2.setFont(new Font("Arial",Font.BOLD, 14));
+        add(textField2);
 
+        label3 = new JLabel("PIN:");
+        label3.setFont(new Font("Ralway", Font.BOLD, 28));
+        label3.setForeground(Color.WHITE);
+        label3.setBounds(150, 250, 375, 30);
+        add(label3);
+
+        passwordField3 = new JPasswordField(15);
+        passwordField3.setBounds(325, 250, 230, 30);
+        passwordField3.setFont(new Font("Arial", Font.BOLD,14));
+        add(passwordField3);
+
+        button1 = new JButton("SIGN IN");
+        button1.setFont(new Font("Arial", Font.BOLD, 14));
+        button1.setForeground(Color.WHITE);
+        button1.setBackground(Color.BLACK);
+        button1.setBounds(300, 300, 100, 30);
+        button1.addActionListener(this);
+        add(button1);
+
+        button2 = new JButton("CLEAR");
+        button2.setFont(new Font("Arial", Font.BOLD, 14));
+        button2.setForeground(Color.WHITE);
+        button2.setBackground(Color.BLACK);
+        button2.setBounds(430, 300, 100, 30);
+        button2.addActionListener(this);
+        add(button2);
+
+        button3 = new JButton("SIGN UP");
+        button3.setFont(new Font("Arial", Font.BOLD, 14));
+        button3.setForeground(Color.WHITE);
+        button3.setBackground(Color.BLACK);
+        button3.setBounds(300, 350, 230, 30);
+        button3.addActionListener(this);
+        add(button3);
 
         ImageIcon iii1 = new ImageIcon(ClassLoader.getSystemResource("icons/backbg.png"));
         Image iii2 = iii1.getImage().getScaledInstance(850, 480, Image.SCALE_DEFAULT);
@@ -45,13 +88,15 @@ public class Login extends JFrame {
         iiimage.setBounds(0, 0, 850, 480);
         add(iiimage);
 
-
-
         setLayout(null);
         setSize(850, 480);
         setLocation(280, 160);
-
         setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 
